60a083d9b633cb8d67dee29aad8981ae79c306c7
Larry Tooley <larry@larrytooley.com>
2016-03-31 18:55:27 -0400

    Fix pattern add scale variable

--- PyTraffic.py
+++ PyTraffic.py
@@ -12,6 +12,9 @@ secondLightRed = 26
 
 pinIn = 18
 
+scale = 0.5
+
+# set board and warnings
 GPIO.setmode(GPIO.BCM)  # set board mode to Broadcom
 GPIO.setwarnings(False)
 
@@ -31,37 +34,37 @@ GPIO.setup(pinIn, GPIO.IN)
 try:
     while True:
         if GPIO.input(pinIn):
-            sleep(5)
+            sleep(5 * scale)
 
             # first light yellow pattern
-            GPIO.output([firstLightRed, secondLightYellow], GPIO.HIGH)
-            GPIO.output([firstLightGreen, firstLightYellow,
-                         secondLightGreen, secondLightRed], GPIO.LOW)
-            sleep(4)
+            GPIO.output([firstLightYellow, secondLightRed], GPIO.HIGH)
+            GPIO.output([firstLightGreen, firstLightRed,
+                         secondLightGreen, secondLightYellow], GPIO.LOW)
+            sleep(4 * scale)
 
             # first light red pattern
             GPIO.output([firstLightRed, secondLightRed], GPIO.HIGH)
             GPIO.output([firstLightGreen, firstLightYellow,
                          secondLightGreen, secondLightYellow], GPIO.HIGH)
-            sleep(2)
+            sleep(2 * scale)
 
             # second light green pattern
             GPIO.output([firstLightRed, secondLightGreen], GPIO.HIGH)
             GPIO.output([firstLightGreen, firstLightYellow,
                          secondLightYellow, secondLightRed], GPIO.LOW)
-            sleep(10)
+            sleep(10 * scale)
 
             # second light yellow pattern
             GPIO.output([firstLightRed, secondLightYellow], GPIO.HIGH)
             GPIO.output([firstLightYellow, firstLightGreen,
                          secondLightGreen, secondLightRed], GPIO.LOW)
-            sleep(4)
+            sleep(4 * scale)
 
             # second light red pattern
             GPIO.output([firstLightRed, secondLightRed], GPIO.HIGH)
             GPIO.output([firstLightYellow, firstLightGreen,
                          secondLightYellow, secondLightGreen], GPIO.LOW)
-            sleep(2)
+            sleep(2 * scale)
         else:
             # default pattern
             GPIO.output([firstLightGreen, secondLightRed], GPIO.HIGH)
